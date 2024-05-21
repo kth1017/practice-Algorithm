46c7e38dc7b74183fecf6831b4fb0935772bdcf6
kth <rnjsxo1017@gmail.com>
2024-05-21 18:49:14 +0900

    0521

---
+++ src/Prac2.java
@@ -0,0 +1,24 @@
+public class Prac2 {
+    public static void main(String[] args) {
+        int n = 25;
+        int k = 3;
+        int result = 0;
+
+        while (true) {
+            int target = (n / k) * k;
+            result += (n - target);
+            n = target;
+
+            if (n < k) {
+                break;
+            }
+
+            result++;
+            n /= k;
+        }
+
+        result += (n - 1);
+        System.out.println(result);
+    }
+
+}
---
+++ src/Prac3.java
@@ -0,0 +1,47 @@
+public class Prac3 {
+    public static void main(String[] args) {
+        int n = 15;
+        int m = 12;
+        int[] parent = {
+            0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5
+        };
+
+        System.out.println(lca(3, 4, parent));
+        System.out.println(lca(6, 11, parent));
+        System.out.println(lca(13, 9, parent));
+        System.out.println(lca(7, 10, parent));
+    }
+
+    public static int lca(int a, int b, int[] parent) {
+        int[] pathA = new int[100];
+        int[] pathB = new int[100];
+        int indexA = 0;
+        int indexB = 0;
+
+        while (true) {
+            pathA[indexA++] = a;
+            if (a == 0) {
+                break;
+            }
+            a = parent[a];
+        }
+
+        while (true) {
+            pathB[indexB++] = b;
+            if (b == 0) {
+                break;
+            }
+            b = parent[b];
+        }
+
+        for (int i = 0; i < indexA; i++) {
+            for (int j = 0; j < indexB; j++) {
+                if (pathA[i] == pathB[j]) {
+                    return pathA[i];
+                }
+            }
+        }
+
+        return -1;
+    }
+}
