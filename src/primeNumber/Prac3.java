6cd69bc04685a54e01dc2cbc2458317b606cd323
kth <rnjsxo1017@gmail.com>
2024-06-09 21:29:08 +0900

    0609

--- src/primeNumber/Prac3.java
+++
@@ -1,9 +0,0 @@
-package primeNumber;
-
-public class Prac3 {
-    // 에라토스테네스의 체 사용
-    public static void main(String[] args) {
-
-    }
-
-}
--- src/review/StreamPrac3.java
+++ src/review/StreamPrac3.java
@@ -1,20 +1,48 @@
 package review;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamPrac3 {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Student> students = Arrays.asList(
+                new Student("John", 85),
+                new Student("Jane", 92),
+                new Student("Jack", 77),
+                new Student("Jill", 89)
+        );
 
-        int sum = numbers.stream()
-                .mapToInt(Integer::intValue)
-                .sum();
+        List<Student> topStudents = students.stream()
+                .filter(student -> student.getScore() >= 80)
+                .sorted(Comparator.comparingInt(Student::getScore).reversed())
+                .collect(Collectors.toList());
 
-        System.out.println("Sum: " + sum);
+        topStudents.forEach(System.out::println);
+}
+
+static class Student {
+        private String name;
+        private int score;
+
+        public Student(String name, int score) {
+            this.name = name;
+            this.score = score;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getScore() {
+            return score;
+        }
+
+        @Override
+        public String toString() {
+            return "Student{name='" + name + "', score=" + score + '}';
+        }
+
     }
-
-
-
 }
