15a37b27865f9978eb194e5e2d1992624f487fdb
kth <rnjsxo1017@gmail.com>
2024-06-17 21:47:14 +0900

    0617

--- src/graph/Prac4.java
+++ src/graph/Prac4.java
@@ -1,84 +1,65 @@
 package graph;
 
 public class Prac4 {
-    public static final int INF = (int) 1e9;
-    public static final int[] dx = {-1, 1, 0, 0};
-    public static final int[] dy = {0, 0, -1, 1};
 
+    public static final int INF = (int) 1e9;
     public static void main(String[] args) {
         int n = 5;
         int m = 6;
         int[][] graph = {
-            {1, 0, 1, 0, 1, 0},
-            {1, 1, 1, 1, 1, 1},
-            {0, 0, 0, 0, 0, 1},
-            {1, 1, 1, 1, 1, 1},
-            {1, 1, 1, 1, 1, 1}
+            {INF, 0, 1, INF, 1, INF},
+            {0, INF, 1, 1, 1, 1},
+            {1, 1, INF, 0, 0, 1},
+            {INF, 1, 0, INF, 1, 1},
+            {1, 1, 0, 1, INF, 1}
         };
 
-        System.out.println(bellmanFord(0, 0, n, m, graph));
+        System.out.println(prim(0, n, m, graph));
     }
 
-    public static int bellmanFord(int x, int y, int n, int m, int[][] graph) {
-        int[][] distance = new int[n][m];
+    public static int prim(int start, int n, int m, int[][] graph) {
+        int[] distance = new int[n];
+        boolean[] visited = new boolean[n];
+
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                distance[i][j] = INF;
-            }
+            distance[i] = INF;
         }
 
-        distance[x][y] = 0;
+        distance[start] = 0;
+        visited[start] = true;
 
         for (int i = 0; i < n - 1; i++) {
+            int now = getSmallestNode(n, distance, visited);
+            visited[now] = true;
+
             for (int j = 0; j < n; j++) {
-                for (int k = 0; k < m; k++) {
-                    if (distance[j][k] == INF) {
-                        continue;
-                    }
-
-                    for (int l = 0; l < 4; l++) {
-                        int nx = j + dx[l];
-                        int ny = k + dy[l];
-
-                        if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
-                            continue;
-                        }
-
-                        if (distance[nx][ny] > distance[j][k] + graph[nx][ny]) {
-                            distance[nx][ny] = distance[j][k] + graph[nx][ny];
-                        }
-                    }
+                if (!visited[j] && graph[now][j] != INF) {
+                    distance[j] = Math.min(distance[j], graph[now][j]);
                 }
             }
         }
 
-        return distance[n - 1][m - 1];
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            if (distance[i] != INF) {
+                result += distance[i];
+            }
+        }
+
+        return result;
     }
 
-    static class Node {
-        private final int x;
-        private final int y;
-        private final int distance;
+    public static int getSmallestNode(int n, int[] distance, boolean[] visited) {
+        int min = INF;
+        int index = 0;
 
-        public Node(int x, int y, int distance) {
-            this.x = x;
-            this.y = y;
-            this.distance = distance;
+        for (int i = 0; i < n; i++) {
+            if (distance[i] < min && !visited[i]) {
+                min = distance[i];
+                index = i;
+            }
         }
 
-        public int getX() {
-            return x;
-        }
-
-        public int getY() {
-            return y;
-        }
-
-        public int getDistance() {
-            return distance;
-        }
+        return index;
     }
-
-
-
 }
