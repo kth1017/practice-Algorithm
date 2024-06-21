a70df94de528d113f76e648fcc0398768d2ff2be
kth <rnjsxo1017@gmail.com>
2024-06-21 21:40:43 +0900

    0621

--- src/graph/Prac1.java
+++ src/graph/Prac1.java
@@ -1,53 +1,51 @@
 package graph;
 
 public class Prac1 {
-    // DFS(재귀)
+    // Topological Sorting practice
     public static void main(String[] args) {
-        int n = 5;
-        int m = 6;
+        int n = 7;
         int[][] graph = {
-            {1, 0, 1, 0, 1, 0},
-            {1, 1, 1, 1, 1, 1},
-            {0, 0, 0, 0, 0, 1},
-            {1, 1, 1, 1, 1, 1},
-            {1, 1, 1, 1, 1, 1}
+            {0, 1},
+            {0, 2},
+            {1, 3},
+            {1, 4},
+            {2, 5},
+            {3, 6},
+            {4, 6},
+            {5, 6}
         };
 
-        System.out.println(dfs1(0, 0, n, m, graph));
+        topologicalSort(n, graph);
     }
 
-    public static int dfs1(int x, int y, int n, int m, int[][] graph) {
-        if (x <= -1 || x >= n || y <= -1 || y >= m) {
-            return 0;
+    public static void topologicalSort(int n, int[][] graph) {
+        int[] indegree = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < graph.length; j++) {
+                if (graph[j][1] == i) {
+                    indegree[i]++;
+                }
+            }
         }
 
-        if (graph[x][y] == 0) {
-            graph[x][y] = 1;
-
-            dfs1(x - 1, y, n, m, graph);
-            dfs1(x + 1, y, n, m, graph);
-            dfs1(x, y - 1, n, m, graph);
-            dfs1(x, y + 1, n, m, graph);
-
-            return 1;
-        }
-
-        return 0;
-    }
-
-    static class Node {
-        private final int x;
-        private final int y;
-
-        public Node(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public int getX() {
-            return x;
+        for (int i = 0; i < n; i++) {
+            int x = getZeroIndegree(n, indegree);
+            indegree[x] = -1;
+            System.out.print(x + " ");
+            for (int j = 0; j < graph.length; j++) {
+                if (graph[j][0] == x) {
+                    indegree[graph[j][1]]--;
+                }
+            }
         }
     }
 
-
+    public static int getZeroIndegree(int n, int[] indegree) {
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
